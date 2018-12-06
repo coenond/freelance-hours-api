273a882bab08080016d200fc055df8805373f013
Coen Onderwater <coenonderwater@gmail.com>
2018-12-06 15:10:45 +0100

    feature: add auth, migrations, models and controllers

---
+++ app/Http/Controllers/TaxController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tax;
+use Illuminate\Http\Request;
+
+class TaxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tax  $tax
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tax $tax)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tax  $tax
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tax $tax)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tax  $tax
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tax $tax)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tax  $tax
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tax $tax)
+    {
+        //
+    }
+}
