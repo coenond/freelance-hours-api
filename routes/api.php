e06fd9b16c40b15466b86c9ef430320dd366ea08
Coen Onderwater <coenonderwater@gmail.com>
2018-12-13 11:54:34 +0100

    feature: add endpoits for resources

--- routes/api.php
+++ routes/api.php
@@ -7,38 +7,139 @@ Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
 	Route::post('register', 'RegisterController@make');
 });
 
+/**
+ * User Routes
+ */
 Route::group(['prefix' => 'users'], function() {
-
 	Route::get('/', [
 		'as'     => 'users.index',
 		'uses'   => 'UserController@index'
 	]);
 
-	Route::get('create', [
-		'as'     => 'cms.product.create',
-		'uses'   => 'ProductController@create'
-	]);
-
-	Route::post('/', [
-		'as'     => 'cms.product.store',
-		'before' => 'csrf',
-		'uses'   => 'ProductController@store'
-	]);
-
 	Route::get('{id}/edit', [
-		'as'     => 'cms.product.edit',
-		'uses'   => 'ProductController@edit'
+		'as'     => 'users.edit',
+		'uses'   => 'UserController@edit'
 	])->where('id', '[0-9]+');
 
 	Route::post('{id}', [
-		'as'     => 'cms.product.update',
+		'as'     => 'users.update',
 		'before' => 'csrf',
-		'uses'   => 'ProductController@update'
+		'uses'   => 'UserController@update'
 	])->where('id', '[0-9]+');
 
 	Route::get('{id}/destroy', [
-		'as'     => 'cms.product.destroy',
-		'uses'   => 'ProductController@destroy'
+		'as'     => 'users.destroy',
+		'uses'   => 'UserController@destroy'
+	])->where('id', '[0-9]+');
+});
+
+/**
+ * Project Routes
+ */
+Route::group(['prefix' => 'projects'], function() {
+	Route::get('/', [
+		'as'     => 'projects.index',
+		'uses'   => 'ProjectController@index'
+	]);
+
+	Route::get('create', [
+		'as'     => 'projects.create',
+		'uses'   => 'ProjectController@create'
+	]);
+
+	Route::post('/', [
+		'as'     => 'projects.store',
+		'before' => 'csrf',
+		'uses'   => 'ProjectController@store'
+	]);
+
+	Route::get('{id}/edit', [
+		'as'     => 'projects.edit',
+		'uses'   => 'ProjectController@edit'
 	])->where('id', '[0-9]+');
 
+	Route::post('{id}', [
+		'as'     => 'projects.update',
+		'before' => 'csrf',
+		'uses'   => 'ProjectController@update'
+	])->where('id', '[0-9]+');
+
+	Route::get('{id}/destroy', [
+		'as'     => 'projects.destroy',
+		'uses'   => 'ProjectController@destroy'
+	])->where('id', '[0-9]+');
+});
+
+/**
+ * Taxes Routes
+ */
+Route::group(['prefix' => 'taxes'], function() {
+	Route::get('/', [
+		'as'     => 'taxes.index',
+		'uses'   => 'TaxController@index'
+	]);
+
+	Route::get('create', [
+		'as'     => 'taxes.create',
+		'uses'   => 'TaxController@create'
+	]);
+
+	Route::post('/', [
+		'as'     => 'taxes.store',
+		'before' => 'csrf',
+		'uses'   => 'TaxController@store'
+	]);
+
+	Route::get('{id}/edit', [
+		'as'     => 'taxes.edit',
+		'uses'   => 'TaxController@edit'
+	])->where('id', '[0-9]+');
+
+	Route::post('{id}', [
+		'as'     => 'taxes.update',
+		'before' => 'csrf',
+		'uses'   => 'TaxController@update'
+	])->where('id', '[0-9]+');
+
+	Route::get('{id}/destroy', [
+		'as'     => 'taxes.destroy',
+		'uses'   => 'TaxController@destroy'
+	])->where('id', '[0-9]+');
+});
+
+/**
+ * Activity Routes
+ */
+Route::group(['prefix' => 'activities'], function() {
+	Route::get('/', [
+		'as'     => 'activities.index',
+		'uses'   => 'ActivityController@index'
+	]);
+
+	Route::get('create', [
+		'as'     => 'activities.create',
+		'uses'   => 'ActivityController@create'
+	]);
+
+	Route::post('/', [
+		'as'     => 'activities.store',
+		'before' => 'csrf',
+		'uses'   => 'ActivityController@store'
+	]);
+
+	Route::get('{id}/edit', [
+		'as'     => 'activities.edit',
+		'uses'   => 'ActivityController@edit'
+	])->where('id', '[0-9]+');
+
+	Route::post('{id}', [
+		'as'     => 'activities.update',
+		'before' => 'csrf',
+		'uses'   => 'ActivityController@update'
+	])->where('id', '[0-9]+');
+
+	Route::get('{id}/destroy', [
+		'as'     => 'activities.destroy',
+		'uses'   => 'ActivityController@destroy'
+	])->where('id', '[0-9]+');
 });
